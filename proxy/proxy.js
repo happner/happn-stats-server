99f784311179bc278fbab785cdcae83cb571a72b
nomilous <nomilous@gmail.com>
2017-11-19 22:35:17 +0200

    happn-stats-server

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -1,28 +1,22 @@
 #!/usr/bin/env node
 
-var path = require('path');
-var fs = require('fs');
 var http = require('http');
 var auth = require('http-auth');
 var httpProxy = require('http-proxy');
 
 var basic, proxy, server;
 
-try {
-  // mounted in docker to ./www
-  basic = auth.basic({
-    file: '/var/www/users.htpasswd'
-  });
-} catch (e) {
-  basic = auth.basic({
-    file: path.resolve(__dirname, '..', 'www', 'users.htpasswd')
-  });
-}
+basic = auth.basic({
+  // mounted in docker container
+  file: '/var/www/users.htpasswd'
+});
 
 proxy = httpProxy.createProxyServer();
 
 server = http.createServer(basic, function (req, res) {
   proxy.web(req, res, {
+    // hostname resolvable in docker container
+    // (per docker-compose service name)
     target: 'http://kibana:5601'
   });
 });
